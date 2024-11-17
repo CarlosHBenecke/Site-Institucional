19f1b997adcd66f06228e77e3d1d880ab2523e73
Carlos Henrique Benecke <125740192+CarlosHBenecke@users.noreply.github.com>
2024-11-17 12:41:16 -0300

    Update api-blob.js

--- src/api-blob.js
+++ src/api-blob.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiBlob = axios.create({
-    baseURL: "https://ec2-18-205-89-172.compute-1.amazonaws.com/api/ms-blob"
+    baseURL: "https://ec2-18-205-89-172.compute-1.amazonaws.com/api/ms-blob/blob"
 })
 
 export default apiBlob;
