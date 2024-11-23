c7facc02bc90a01549a36fda989e1b4f88c2afda
Carlos Henrique Benecke <125740192+CarlosHBenecke@users.noreply.github.com>
2024-11-22 22:32:50 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://ec2-3-220-57-220.compute-1.amazonaws.com/api/ms-auth"
+    baseURL: "https://ec2-98-82-209-129.compute-1.amazonaws.com/api/ms-auth"
 })
 
 export default api;
