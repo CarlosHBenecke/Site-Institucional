3594172f18a6e505407ac166ae6aed82885a4e31
Samuel Lucena <80936078+SamLucena@users.noreply.github.com>
2024-11-28 20:30:45 -0300

    Update api-blob.js

--- src/api-blob.js
+++ src/api-blob.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiBlob = axios.create({
-    baseURL: "https://ec2-54-84-26-145.compute-1.amazonaws.com/api/ms-blob/blob"
+    baseURL: "https://qualaboa.servebeer.com/api/ms-blob/blob"
 })
 
 export default apiBlob;
