379fad4daa6e508990cbda91c786de8da7cf404b
Samuel Lucena <80936078+SamLucena@users.noreply.github.com>
2024-11-28 20:30:59 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://ec2-54-84-26-145.compute-1.amazonaws.com/api/ms-auth"
+    baseURL: "https://qualaboa.servebeer.com/api/ms-auth"
 })
 
 export default api;
